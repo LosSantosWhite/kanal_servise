60f01eb57e902d240daef9d7c46cda844602357e
LosSantosWhite <69223894+LosSantosWhite@users.noreply.github.com>
2022-08-02 00:40:57 +0300

    fixed update_model

--- orders/controllers/controller.py
+++ orders/controllers/controller.py
@@ -6,18 +6,11 @@ from orders.models import Order
 def update_model(values: list):
     Order.objects.all().delete()
     for value in values:
-        try:
-            order = Order.objects.get(number=value[1])
-            order.number = value[1]
-            order.usd_cost = value[2]
-            order.rub_cost = value[3]
-            order.shipment_date = value[4]
-        except ObjectDoesNotExist:
-            order = Order.objects.create(**{
-                'number': value[1],
-                'usd_cost': value[2],
-                'rub_cost': value[3],
-                'shipment_date': value[4]
-            })
+        order = Order.objects.create(**{
+            'number': value[1],
+            'usd_cost': value[2],
+            'rub_cost': value[3],
+            'shipment_date': value[4]
+        })
 
         order.save()
